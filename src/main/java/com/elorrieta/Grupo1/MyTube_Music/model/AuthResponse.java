c636c570f63f21fa1c55960589bca383847a4936
OierGago <oier.gagoir@elorrieta-errekamari.com>
2023-10-31 18:58:49 +0100

    Auth Login / Register done and tested

---
+++ src/main/java/com/elorrieta/Grupo1/MyTube_Music/model/AuthResponse.java
@@ -0,0 +1,31 @@
+package com.elorrieta.Grupo1.MyTube_Music.model;
+
+public class AuthResponse {
+	
+	private String login;
+	private String accessToken;
+
+	public AuthResponse() { }
+	
+	public AuthResponse(String login, String accessToken) {
+		this.login = login;
+		this.accessToken = accessToken;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+}
